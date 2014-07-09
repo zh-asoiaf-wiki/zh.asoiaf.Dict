b614bd629fdb14172a40dc39d6fff7a5e4e066d1
chenchenyangll <ccy-8888@hotmail.com>
2014-07-09 22:44:54 +0800

    getAll

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var program = require('commander');
 program
-  .version('0.0.1')
+  .version('0.0.2')
   .option('-c --category [categoryName]', 'category title (without "Category:" prefix)')
   .option('-a --all', 'get all pages')
   .parse(process.argv);
@@ -20,11 +20,108 @@ if (fs.existsSync('config.js')) {
   console.log('Use temp config');
 }
 
+var read = function(data, res) {
+  var pages = data.query.pages;
+  for (var pid in pages) {
+    var page = pages[pid];
+    var title = page.title;
+    var langlinks = page.langlinks;
+    if (langlinks != undefined) {
+      for (var i = 0; i < langlinks.length; ++i) {
+        var lang = langlinks[i];
+        if (lang.lang == 'en') {
+          // TODO: check ERROR 
+          res.dict[title] = lang['*'];
+          break;
+        }
+      }
+      if (res.dict[title] == undefined) {
+        res.noen.push(title);
+      }
+    } else {
+      res.noen.push(title);
+    }
+  }
+  return res;
+};
+
+var writeFile = function(res, filename) {
+  console.log('Start writing into file...');
+  var lineBreak = '\r\n'; // TODO: take care of line break
+  if (res.dict != undefined) {
+    var out = fs.createWriteStream(filename + '.txt');
+    for (var item in res.dict) {
+      var line = res.dict[item] + '#' + item + lineBreak;
+      out.write(line);
+    }
+    out.end(function() {
+      console.log('Done.');
+    });
+  }
+  if (res.noen.length != 0) {
+    var out = fs.createWriteStream(filename + '-noen.txt');
+    for (var i = 0; i < res.noen.length; ++i) {
+      out.write(res.noen[i] + lineBreak);
+    }
+    out.end(function() {
+      console.log('Done.');
+    });
+  }
+  if (res.error != undefined) {
+    var out = fs.createWriteStream(filename + '-error.txt');
+    for (var item in res.error) {
+      var line = res.error[item] + '#' + item + lineBreak;
+      out.write(line);
+    }
+    out.end(function() {
+      console.log('Done.');
+    });  
+  }
+};
+
 client.logIn(function() {
   if (program.all) {
+    var res = {
+      'dict': {}, 
+      'noen': [], 
+      'error': {}
+    };
+    var allParams = {
+      action: 'query', 
+      generator: 'allpages', 
+      gaplimit: '1000', 
+      prop: 'langlinks', 
+      lllimit: '5000', 
+      format: 'jsonfm'
+    };
     
+    var callApi = function(allPagrams, callback) {
+      client.api.call(allParams, callback);
+    };
+    var callback = function(info, next, data) {
+      if (!data.query) {
+        console.log('Error or warning occured, plz check parameters again.');
+      } else {
+        if (data['query-continue'] != undefined) {
+          read(data, res);
+          console.log('query-continue');
+          allParams.gapfrom = data['query-continue'].allpages.gapfrom;
+          callApi(allParams, callback);
+        } else {
+          read(data, res);
+          writeFile(res, 'dict-all');
+        }
+      }    
+    };
+    callApi(allParams, callback);
   }
   if (program.category) {
+    var res = {
+      'dict': {}, 
+      'noen': [], 
+      'error': {}
+    };  
+    // TODO: cmcontinue
     var categoryParams = {
       action: 'query', 
       generator: 'categorymembers', 
@@ -34,43 +131,14 @@ client.logIn(function() {
       lllimit: '5000', 
       format: 'jsonfm'
     };
+    
     client.api.call(categoryParams, function(info, next, data) {
-      console.log(info);
-      console.log(next);
       if (!data.query) {
         console.log('Error or warning occured, plz check parameters again.');
       } else {
-        var res = {};
-        var pages = data.query.pages;
-        for (var pid in pages) {
-          var page = pages[pid];
-          var title = page.title;
-          var langlinks = page.langlinks;
-          if (langlinks != undefined) {
-            for (var i = 0; i < langlinks.length; ++i) {
-              var lang = langlinks[i];
-              if (lang.lang == 'en') {
-                res[title] = lang['*'];
-                break;
-              }
-            }
-          }
-        }
-        
-        var out = fs.createWriteStream('dict-' + program.category + '.txt');
-        for (var item in res) {
-          var line = res[item] + '#' + item + '\r\n';
-          out.write(line);
-        }
-        out.end(function() {
-          console.log('Done.');
-        });
+        var dict = read(data, res);
+        writeFile(dict, 'dict-' + program.category);
       }
     });
   }
 });
-
-
-
-
-
